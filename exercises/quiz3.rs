4a00c94235729409a3e0e0beb309839bc62d3b0d
yuluo <yuluozy@outlook.com>
2023-10-10 15:04:23 +0000

    task

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,7 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub struct ReportCard {
     pub grade: f32,
@@ -26,6 +25,10 @@ pub struct ReportCard {
 
 impl ReportCard {
     pub fn print(&self) -> String {
+        if self.grade == 5.0 {
+            return format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, "A+")
+        } 
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
@@ -52,7 +55,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 5.0,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
